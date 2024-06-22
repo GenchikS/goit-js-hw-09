a178086011ff695225c081eeb1451c95ebc8e1d5
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-23 02:01:57 +0300

    v.9.1.5

---
+++ src/js/2-form.js
@@ -0,0 +1,19 @@
+const section = document.querySelector("section");
+
+// section.insertAdjacentHTML('beforeend', createForm());
+
+// function createForm(arr) {
+//     const form = (`
+//     <form class="feedback-form" autocomplete="off">
+//     <label class="label-email">
+//         Email
+//       <input class="input-email" type="email" name="email" autofocus />
+//     </label>
+//     <label class="label-massage">
+//         Message
+//       <textarea class="input-massage" name="message" rows="8"></textarea>
+//     </label>
+//     <button class="button-submit" type="submit">Submit</button>
+//   </form> 
+//   `)}
+
