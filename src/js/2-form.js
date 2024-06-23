e7948d95ffcaecd2800e4a72efb77b27a620aa04
Gennadii_S <106490249+GenchikS@users.noreply.github.com>
2024-06-23 13:57:39 +0300

    v.9.1.6 (2-form start)

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,71 @@
-const section = document.querySelector("section");
+const LS_KEY = "feedback-form-state";
+let formData = { email: "", message: "" };
+// localStorage.setItem(LS_KEY, JSON.stringify(formData))  //  створення ключа в LS
 
-// section.insertAdjacentHTML('beforeend', createForm());
+// const dataInput = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};  //  отримання ключа
+// console.log(`dataInput`, dataInput);  //  виведення ключа
 
-// function createForm(arr) {
-//     const form = (`
-//     <form class="feedback-form" autocomplete="off">
-//     <label class="label-email">
-//         Email
-//       <input class="input-email" type="email" name="email" autofocus />
-//     </label>
-//     <label class="label-massage">
-//         Message
-//       <textarea class="input-massage" name="message" rows="8"></textarea>
-//     </label>
-//     <button class="button-submit" type="submit">Submit</button>
-//   </form> 
-//   `)}
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("submit", inputForm);
+
+function inputForm(event) {
+    event.preventDefault();
+    const { email, message } = event.currentTarget.elements;
+    formData.email = email.value;
+    formData.message = message.value;
+    console.log(`emailUser`, formData.email);
+    console.log(`messagelUser`, formData.message);
+    formData = {
+        email: formData.email,
+        message: formData.message
+    };
+    console.log("formData", formData);
+}
+
+
+
+// const inputEmailUser = document.querySelector(".input-email");
+
+// inputEmailUser.addEventListener(`input`, inputEmail);
+
+// function inputEmail(event) {
+//     formData.email = event.currentTarget.value;
+//     localStorage.setItem(LS_KEY, JSON.stringify(formData))  //  створення ключа в LS email
+    
+// }
+
+// const inputMassageUser = document.querySelector(".input-massage");
+// inputMassageUser.addEventListener(`input`, inputMassage);
+
+// function inputMassage(event) {
+//     formData.message = event.currentTarget.value;
+//     localStorage.setItem(LS_KEY, JSON.stringify(formData))  //  створення ключа в LS для message
+// }
+
+// const button = document.querySelector("button");
+// console.log(`button`,button)
+// button.addEventListener(`submit`, buttonSubmit);
+
+// function buttonSubmit() {
+//     console.log(`button`, button);
+//     // const dataInput = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};
+//     // console.log(`dataInput`, dataInput);
+//     // if (event.currentTarget.value.trim() === "") {
+//     //     alert("Fill please all fields");
+//     // }
+//     const dataInput = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};  //  отримання ключа
+//     console.log(`dataInput`, dataInput);  //  виведення ключа
+//     formData.clear();
+// }
+
+// const dataInput = JSON.parse(localStorage.getItem(LS_KEY)) ?? {};  //  отримання ключа
+// console.log(`dataInput`, dataInput);  //  виведення ключа
+
+
+
+
+
+
 
